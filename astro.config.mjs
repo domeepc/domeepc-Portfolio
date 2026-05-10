b6120430a188334b0ccb5bed197c35b7784a6a1c
domeepc <domagoj.milardovic@gmail.com>
2026-05-10 17:23:55 +0200

    logo fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import path from 'path';
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: '/domeepc-Portfolio',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
